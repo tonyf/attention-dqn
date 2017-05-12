a0badcc990f12ac60ac618cdc45a718c8a01fb9f
Tony Francis <tonyfrancisv@gmail.com>
2017-05-12 19:48:00 -0400

    sigh

--- dqn.py
+++ dqn.py
@@ -8,14 +8,11 @@ class DQN(nn.Module):
         self.conv1 = nn.Conv2d(4, out_channels=32, kernel_size=(8,8), stride=4)
         self.conv2 = nn.Conv2d(32, out_channels=64, kernel_size=(4,4), stride=2)
         self.conv3 = nn.Conv2d(64, out_channels=64, kernel_size=(1,1), stride=1)
-        self.fc1   = nn.Linear(64, 512)
-        self.fc3   = nn.Linear(512, num_actions)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.head = nn.Linear(576, num_actions)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 64)
-        x = F.relu(self.fc1(x))
-        x = self.fc3(x)
-        return x
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
--- envs/attention_env.py
+++ envs/attention_env.py
@@ -7,7 +7,7 @@ import numpy as np
 import sys
 import math
 
-SIZE=24
+SIZE=40
 
 class AttentionEnv:
     metadata = {'render.modes': ['human', 'array']}
